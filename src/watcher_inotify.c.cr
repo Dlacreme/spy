fa1e03236026d1b7e5fad59935b2ed18377e11f5
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2020-03-21 15:35:18 +0900

    fix get_fds. Use struct instead of unio n

--- src/watcher_inotify.c.cr
+++ src/watcher_inotify.c.cr
@@ -2,7 +2,7 @@ module Spy::Watcher
   # inotify API is the lowest API to listen for change events on files & folders
   # Reference: INOTIFY (7)
   lib C
-    union Fds
+    struct Fds
       fd : Int32
       events : Int16
       revents : Int16
@@ -25,16 +25,12 @@ module Spy::Watcher
 
     def register(target : String, &block)
       fd = C.inotify_init1(C::IN_NONBLOCK)
-      puts "FD = #{fd}"
       raise Exception.new("INOTIFY not available") if fd == -1
       watched = C.inotify_add_watch(fd, target, C::IN_MODIFY)
-      puts "WATCHED = #{watched}"
       fds = get_fds(fd)
-      # fds.each { |x| puts("FD: #{x.fd} ; EVENTS: #{x.events} ; REVENTS: #{x.revents}") }
       loop do
         puts "Start watching '#{target}'"
         poll_num = C.poll(fds, fds.size, -1)
-        puts "POLL NUM > #{poll_num}"
         raise Exception.new("Poll error") if poll_num == -1
         yield if poll_num > 0
       end
@@ -42,14 +38,13 @@ module Spy::Watcher
 
     private def get_fds(fd) : Array(C::Fds)
       console_input = C::Fds.new
-      console_input.fd = 0
-      console_input.events = 1
-      # puts("FD: #{console_input.fd} ; EVENTS: #{console_input.events} ; REVENTS: #{console_input.revents}")
+      console_input.fd = C::STDIN_FILENO
+      console_input.events = C::POLLIN
+
       inotify_input = C::Fds.new
       inotify_input.fd = fd
       inotify_input.events = C::POLLIN
-      puts fd
-      puts("FD: #{inotify_input.fd} ; EVENTS: #{inotify_input.events} ; REVENTS: #{inotify_input.revents}")
+
       [console_input, inotify_input] of C::Fds
     end
   end
